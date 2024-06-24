a524cdf40eed2efee8b50d23d74cad5fe80c8582
JagadeeshReddyG <Jagadeeshreddy.gd@gmail.com>
2024-06-24 13:22:15 +0530

    Added Hello Jagadeesh Reddy Welcome To

--- app/src/main/java/com/example/mydummyapkfileandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/mydummyapkfileandroidproject/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello Jagadeesh Reddy Welcome To $name!",
         modifier = modifier
     )
 }
