85220675e51a090db9d39e7cd36bf9546daa2085
JagadeeshReddyG <Jagadeeshreddy.gd@gmail.com>
2024-06-26 09:14:31 +0530

    name Change

--- app/src/main/java/com/example/mydummyapkfileandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/mydummyapkfileandroidproject/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello JReddy Welcome To $name!",
+        text = "Hello jagadeesh Welcome To $name!",
         modifier = modifier
     )
 }
